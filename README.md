8600ee72601d54aa9a7f89abccade14ce846f7da
wander <hamzaotu1@gmail.com>
2024-08-15 13:04:16 +0100

    add links, specofy language, limit news, add subject, control category

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project?
+this is about getting news from an api and send it to yourself via email
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import requests
+from send_email import send_email
+
+topic = "tesla"
 
 api_key = "bafe0573b42b4cc096ebdd7dfb5dd261"
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-07-15&sortBy=publishedAt&apiKey"
-       "=bafe0573b42b4cc096ebdd7dfb5dd261")
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&from=2024-07-15&" \
+      "sortBy=publishedAt&" \
+      "apiKey=bafe0573b42b4cc096ebdd7dfb5dd261&" \
+      "language=en"
 
 request = requests.get(url)
-content = request.text
 
+content = request.json()
+
+body =""
+for article in content["articles"][:20]:
+    body = ("Subject: Today's news" + "\n"
+            + (body + article["title"] + "\n"
+            + str(article["description"]) + "\n"
+            + article["url"] + 2*"\n"))
+
+body = body.encode("utf-8")
+send_email(message=body)
